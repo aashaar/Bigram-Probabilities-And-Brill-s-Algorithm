9baa5dd951af41fc2c18b15a041ddf30c62d4c87
Aashaar Panchalan <aashaar@outlook.com>
2019-02-18 12:36:47 -0600

    calculated marginal probabilities- added function 'calculateMarginalProbabilities'.

--- src/Q2.java
+++ src/Q2.java
@@ -16,6 +16,8 @@ public class Q2
         int distinctBigramTotalCount = 0;
         HashMap<String, Integer> unigramsMap = new HashMap<String, Integer>();
         HashMap<String, Integer> bigramsMap = new HashMap<String, Integer>();
+        HashMap<String,Double> unigramMarginalProbabilities = new HashMap<String, Double>();
+        HashMap<String,Double> bigramMarginalProbabilities = new HashMap<String, Double>();
         // create an output file
         try
         {
@@ -25,7 +27,7 @@ public class Q2
             File file = new File(args[0]);
             //user delimitter (space-.-space-space 0r .-space-'') :
             Scanner s = new Scanner(file).useDelimiter(" .  |. ''");
-            int n =5;
+            int n =1;
             while(n!=0)
             //while(s.hasNext())
             {
@@ -34,7 +36,7 @@ public class Q2
                 String s1 = s.next() + " .";
                 //System.getProperty("line.terminator") to get the line break in any OS:
                 s1 = s1.replace(System.getProperty("line.separator")," ");
-                System.out.println(s1);
+                //System.out.println(s1);
                 // split by white spaces & tab :
                 for (String token : s1.split("  | |\\t|\\r"))
                 {
@@ -56,7 +58,7 @@ public class Q2
                     if(previousToken != null)
                     {
                         bigramsTotalCount++;
-                        String pair = previousToken + " " + token;
+                        String pair = previousToken + "~~~" + token;
                         if(bigramsMap.containsKey(pair))
                         {
                             int pairCount = bigramsMap.get(pair);
@@ -72,26 +74,58 @@ public class Q2
                 }
                 previousToken = null;
             }
+
+            //calculate marginal probabilities:
+            //for unigrams:
+            unigramMarginalProbabilities = calculateMarginalProbabilities(unigramsMap,unigramsTotalCount);
+            //for bigrams:
+            bigramMarginalProbabilities = calculateMarginalProbabilities(bigramsMap,bigramsTotalCount);
+
             s.close();
-            System.out.println(unigramsTotalCount);
-            System.out.println(distinctUnigramTotalCount);
-            System.out.println(bigramsTotalCount);
-            System.out.println(distinctBigramTotalCount);
-            printHashMap(unigramsMap);
+            //System.out.println(unigramsTotalCount);
+            //System.out.println(distinctUnigramTotalCount);
+            //System.out.println(bigramsTotalCount);
+            //System.out.println(distinctBigramTotalCount);
+            //printHashMap(unigramsMap);
+            printHashMap1(unigramMarginalProbabilities);
+
+            printHashMap1(bigramMarginalProbabilities);
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
     }
-    static void printHashMap(HashMap<String, Integer> map)
+    // function to print HashMaps:
+    private static void printHashMap(HashMap<String, Integer> map)
     {
         Iterator<String> itr = map.keySet().iterator();
         while (itr.hasNext())
         {
             String key = itr.next();
             int value = map.get(key);
-            System.out.println(key + " - "+ value);
+            System.out.println(key + " : "+ value);
         }
+    }
+
+    private static void printHashMap1(HashMap<String, Double> map)
+    {
+        Iterator<String> itr = map.keySet().iterator();
+        while (itr.hasNext())
+        {
+            String key = itr.next();
+            double value = map.get(key);
+            System.out.println(key + " : "+ value);
+        }
+    }
+
+    private static HashMap<String, Double> calculateMarginalProbabilities(HashMap<String, Integer> map, int totalCount)
+    {
+        HashMap<String, Double> result = new HashMap<String, Double>();
+        for(Map.Entry<String,Integer> entry : map.entrySet())
+        {
+            result.put(entry.getKey(), (double) entry.getValue()/totalCount);
+        }
+        return  result;
     }
 }
